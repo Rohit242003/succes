998299f47ce1cf0e2f397fd9b4140a475bb1cc1a
Rohit242003 <155900331+Rohit242003@users.noreply.github.com>
2024-01-18 18:16:17 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,30 @@
+const express=require("express");
+const cors=require("cors");
+const mysql=require("mysql2");
+
+const con =mysql.createConnection({
+
+host:"sql12.freesqldatabase.com",
+user:"sql12677866",
+password:"3dETljLjGp",
+database:"sql12677866"
+
+
+});
+
+const app= express();
+app.use(cors());
+app.use(express.json());
+
+app.post("/save",(req,res)=>{
+let data =[req.body.name,req.body.company,req.body.pkg];
+let sql ="insert into student values(?,?,?)";
+con.query(sql,data,(err,result)=>{
+if(err) res.send(err);
+else res.send(result);
+
+});
+
+
+});
+app.listen(5000,()=>{console.log("ready@5000");});
